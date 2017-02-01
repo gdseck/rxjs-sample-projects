3af1f130bf31d4eac4863e150a705e7ffb8dc403
Gregory Deseck <Gregory_Deseck@applied-maths.com>
2017-02-01 13:03:32 +0100

    Setup

--- server.js
+++ server.js
@@ -1,30 +1,28 @@
 var express = require('express')
 var logger = require('morgan')
 const path = require('path')
-const pug = require('pug')
 const nib = require('nib')
 const stylus = require('stylus')
 var app = express()
 
 const compile = (str, path) => {
-  return stylus(str)
-    .set('filename', path)
-    .use(nib())
+  return stylus(str).set('filename', path).use(nib())
 }
 
-// var template = require('jade').compileFile(__dirname + '/index.jade')
-app.set('views', './views')
+app.set('views', 'src/views')
 app.set('view engine', 'pug')
 
-app.use(stylus.middleware({
-  src: path.join(__dirname, '/styles'),
-  dest: path.join(__dirname, '/static'),
-  compile: compile
-}))
+app.use(
+  stylus.middleware({
+    src: path.join(__dirname, '/styles'),
+    dest: path.join(__dirname, '/static'),
+    compile: compile
+  })
+)
 
 app.use(logger('dev'))
-app.use(express.static(__dirname + '/styles'))
-app.use(express.static(__dirname + '/js'))
+app.use('/assets/', express.static(path.resolve(__dirname, 'src')))
+app.use(express.static('build'))
 
 app.get('/', function (req, res, next) {
   try {
@@ -34,7 +32,13 @@ app.get('/', function (req, res, next) {
   }
 })
 
-app.get('/')
+app.get('/lettercount', function (req, res, next) {
+  try {
+    res.render('lettercount')
+  } catch (e) {
+    next(e)
+  }
+})
 
 app.listen(3000, function () {
   console.log('node server running on port 3000')
--- src/js/lettercount.js
+++ src/js/lettercount.js
@@ -1,17 +1,17 @@
-var Rx = require('rxjs')
-
 (function () {
+  var Rx = require('rxjs')
   const $toCount = document.querySelector('#toCount')
   const $result = document.querySelector('#result')
 
-  const source = Rx.Observable.fromEvent($toCount, 'keyup')
-    .map((e) => `length: ${e.target.value.length}`)
+  const source = Rx.Observable
+    .fromEvent($toCount, 'keyup')
+    .map(e => `length: ${e.target.value.length}`)
     .distinctUntilChanged()
 
-  const setHtml = (text) => {
+  const setHtml = text => {
     console.log(text)
     this.innerHtml = text
   }
 
   source.subscribe(setHtml.bind($result))
-}())
+})()
